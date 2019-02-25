397ec965cbf7bb001f0b6137bfb56cf0eb81c092
Matthew Paletta <mattpaletta@gmail.com>
2019-02-25 14:01:24 -0800

    Added tests

--- colouring/traversal.py
+++ colouring/traversal.py
@@ -13,18 +13,22 @@ class Colour(Enum):
 
 
 class Node(object):
-    def __init__(self, colour: Colour):
+    def __init__(self, colour: Colour, id: int):
         self._colour = colour
+        self.id = id
 
     def is_colour(self, colour: Colour) -> bool:
-        return self._colour.value == colour.value
+        return self.get_colour().value == colour.value
+
+    def get_colour(self) -> Colour:
+        return self._colour
 
 
-def traverse_graph(graph: Dict[Node, List[Node]], node_colour: Dict[Node, Colour], target_colours: List[Colour]) \
+def traverse_graph(graph: Dict[Node, List[Node]], target_colours: List[Colour]) \
         -> Iterator[Dict[Node, List[Node]]]:
 
     # Lookup all nodes with that same colour
-    node_colour_lookup: Dict[Colour, List[Node]] = {}
+    node_colour_lookup: Dict[Colour, List[Node]] = build_node_lookup(list(graph.keys()))
 
     def find_children(starting_node: Node,
                       has_visited: List[Node],
@@ -34,16 +38,16 @@ def traverse_graph(graph: Dict[Node, List[Node]], node_colour: Dict[Node, Colour
         # Get all children of root.
         children_of_root = graph.get(starting_node, [])
         for child in children_of_root:
-            child_name = child.__name__
+            child_name = child.id
             if child_name in has_visited:
-                logging.debug("Skipping visited child: " + child_name)
+                print("Skipping visited child: {0}".format(child_name))
                 continue
-            if child_name == starting_node.__class__.__name__:
-                logging.debug("Skipping duplicate node:" + child_name)
+            if child_name == starting_node.id:
+                print("Skipping duplicate node: {0}".format(child_name))
                 continue
 
             if child in remaining_targets:  # We found one of our target nodes!
-                logging.debug("Found a child node: " + child_name)
+                print("Found a child node: {0}".format(child_name))
 
                 # Remove child from target nodes, and recurse
                 remaining_children = [x for x in remaining_targets if x != child]
@@ -66,19 +70,36 @@ def traverse_graph(graph: Dict[Node, List[Node]], node_colour: Dict[Node, Colour
                                          remaining_targets = remaining_children,
                                          curr_adj = new_adj)
 
-    starting_nodes = __get_all_starting_nodes(target_colours, node_colour_lookup)
+    starting_nodes = get_all_starting_nodes(targets = target_colours,
+                                            node_colours = node_colour_lookup)
 
     green_nodes = node_colour_lookup.get(Colour.GREEN, [])
-    if len(green_nodes) > 1:
+    if len(green_nodes) >= 1:
         for green in green_nodes:
             for target_nodes in starting_nodes:
                 yield from find_children(starting_node = green,
-                                 has_visited = [green],  # Make sure we don't go in circles
-                                 remaining_targets = target_nodes,
-                                 curr_adj = {})
+                                         has_visited = [green],  # Make sure we don't go in circles
+                                         remaining_targets = target_nodes,
+                                         curr_adj = {})
 
 
-def __get_all_starting_nodes(targets: List[Colour], all_node_colours: Dict[Colour, List[Node]]) -> Iterator[List[Node]]:
+def build_node_lookup(nodes: List[Node]) -> Dict[Colour, List[Node]]:
+    # Given a graph, return a dictionary containing a key as a colour, and a list of nodes that are that colour.
+
+    output: Dict[Colour, List[Node]] = {}
+
+    for node in nodes:
+        node_colour: Colour = node.get_colour()
+
+        # Append it to the other nodes we have with that colour, get the empty list if this is the first one.
+        output.update({node_colour: [node] + output.get(node_colour, [])})
+
+    return output
+
+
+def get_all_starting_nodes(targets: List[Colour],
+                           node_colours: Dict[Colour, List[Node]]) -> Iterator[List[Node]]:
+
     # Get all possible combination of Nodes that are Colour
     # given -> [[A], [B], [C, D]]
     # output1 -> [A, B, C]
@@ -88,13 +109,13 @@ def __get_all_starting_nodes(targets: List[Colour], all_node_colours: Dict[Colou
     def join(sub_plan_data: List[Colour]) -> Iterator[List[Node]]:
         if len(sub_plan_data) == 0:
             yield []
-        elif len(sub_plan_data) == 1:
-            yield list(self._goal_input_adj.get(sub_plan_data[0].name))
+        elif len(sub_plan_data) == 1 and len(targets) > 1:
+            yield list(node_colours.get(sub_plan_data[0]))
         else:
-            current_plan_data: targets = sub_plan_data.pop()
+            current_colour = sub_plan_data.pop()
             # Join this one with the yielded result from the previous
             for sub_plans in join(sub_plan_data):
-                for current_data_node in self._goal_input_adj.get(self.__value_class_to_str(current_plan_data.name), []):
+                for current_data_node in node_colours.get(current_colour, []):
                     yield sub_plans + [current_data_node]
 
     yield from join(sub_plan_data = targets)
--- test/test_traversal.py
+++ test/test_traversal.py
@@ -1,10 +1,27 @@
 from unittest import TestCase
-from colouring.traversal import traverse_graph, Node, Colour
+from colouring.traversal import traverse_graph, Node, Colour, get_all_starting_nodes, build_node_lookup
 
 
 class TestTraversal(TestCase):
     def test_empty_graph(self):
         result = traverse_graph(graph = {},
-                                node_colour = {},
                                 target_colours = [])
         assert list(result) == [], "Empty graph returns empty list."
+
+    def test_single_graph(self):
+        simple_graph = {
+            Node(colour = Colour.GREEN, id = 1): [Node(colour = Colour.BLACK, id = 2)],
+            Node(colour = Colour.BLACK, id = 2): [Node(colour = Colour.BLUE, id = 3)],
+            Node(colour = Colour.BLUE, id = 3): []
+        }
+        result = traverse_graph(graph = simple_graph,
+                                target_colours = [Colour.BLUE])
+        print(list(result))
+
+    def small_node_list(self):
+        return [Node(colour = Colour.BLUE, id = 1),
+
+                # Make sure we only have 1 red node.
+                Node(colour = Colour.RED, id = 2),
+                Node(colour = Colour.YELLOW, id = 3),
+                Node(colour = Colour.BLUE, id = 4)]
